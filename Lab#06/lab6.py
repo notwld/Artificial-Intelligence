66e97257a4997dc788838c22ca7739a650a14bf1
Muhammad Waleed <77747037+notwld@users.noreply.github.com>
2023-04-26 07:27:00 +0000

    added lab6

---
+++ Lab#06/lab6.py
@@ -0,0 +1,33 @@
+class Idk:
+    def __init__(self,graph) -> None:
+        self.graph = graph
+
+
+
+graph = {
+    "Ar":[],
+    
+}
+
+h_fun = {
+"Arad": 366
+"Mehadia": 241
+"Bucharest": 0
+"Neamt": 234
+"Craiova": 160
+"Oradea": 380
+"Drobeta": 242
+"Pitesti": 100
+"Eforie": 161
+"Rimnicu Vilcea": 193
+"Fagaras": 176
+"Sibiu": 253
+"Giurgiu": 77
+"Timisoara": 329
+"Hirsova": 151
+"Urziceni": 80
+"Iasi": 226
+"Vaslui": 199
+"Lugoj": 244
+"Zerind": 374
+}
